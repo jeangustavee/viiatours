da270527586f3b5bbcd650ac589940910b6017de
jeangustavee <jean.egustave@gmail.com>
2016-06-23 01:41:10 -0400

    Add header and footer in tour page
    starting working on tour page

--- inc/header.php
+++ inc/header.php
@@ -35,16 +35,16 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a>
+                <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo"></a>
             </div>
             <div class="collapse navbar-collapse navbar-right">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="index.html">Home</a></li>
-                    <li><a href="tours.html">Tours</a></li>
-                    <li><a href="transport.html">Transport</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="contact.html">Contact</a></li>
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="tours.php">Tours</a></li>
+                    <li><a href="transportation.php">Transport</a></li>
+                    <li><a href="company.php">About</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="contact.php">Contact</a></li>
 
                 </ul>
             </div>
