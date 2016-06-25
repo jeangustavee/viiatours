0f2c42c972bdf5484a1554a516f14bf05142b7f6
Kenold Beauplan <bkenold@gmail.com>
2016-06-25 00:34:04 -0400

    duplicate tour row

--- tours.php
+++ tours.php
@@ -17,7 +17,9 @@
         <img class="img-responsive center-block" src="images/tours/cover_img/tours-cover-img.png" alt="">
     <hr>
 
-    <!-- ---------------------- TOURS ROW 1-->
+    <!-- ----------------------
+    TOURS ROW 1
+    --------------------------->
     <div class="row" id="tours">
         <div class="col-sm-3">
             <div class="tours-thumb">
@@ -43,6 +45,35 @@
     </div> <!-- /.row #tours -->
 
     <!-- ---------------------- END TOURS ROW 1-->
+
+    <!-- ----------------------
+    TOURS ROW 2
+    --------------------------->
+    <div class="row" id="tours">
+        <div class="col-sm-3">
+            <div class="tours-thumb">
+                <a href="#"><img src="images/tours/snorkeling/horse-riding-1.jpg" class="img-responsive" alt=""></a>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <div class="tours-middle">
+                <h3>Horseback Riding</h3>
+                <p>Live the dream of riding a horse along tropical white-sand beaches on this day trip from Punta Cana! Gorgeous scenery, friendly guides and amazing horses make this trip a trip to remember. Designed for all ages and all levels of experience, this fun horse-riding adventure is...  </p>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="tours-price">
+                <span class="tour-deal">Deal</span>
+                <p>from USD</p>
+                <h2><center>$63</center></h2>
+                <p><a class="btn btn-blue btn-md" href="#" role="button">BOOK NOW</a></p>
+            </div>
+        </div>
+    </div> <!-- /.row #tours -->
+
+    <!-- ---------------------- END TOURS ROW 2-->
 
 </div> <!-- /. container -->
 
