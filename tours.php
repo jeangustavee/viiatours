db9cfe0fffc659adcc802983b71e8a6b7beb230c
Kenold Beauplan <bkenold@gmail.com>
2016-06-24 23:04:22 -0400

    add div blocks

--- tours.php
+++ tours.php
@@ -20,21 +20,25 @@
     
    <!-- TOURS -->
     <div class="row" id="tours">
-    <div class="col-sm-3">
-        <a href="#">
-        <img src="images/tours/snorkeling/tour-saona.png" class="img-responsive" alt="">
-            </a>
+        <div class="tours-thumb"></div>
+            <div class="col-sm-3">
+                <a href="#"><img src="images/tours/snorkeling/tour-saona.png" class="img-responsive" alt=""></a>
+            </div>
+        </div>
+
+        <div class="tours-middle">
+            <div class="col-sm-6" style="padding:10px">
+                <p><font size="5px">SNORKELIING ADVENTURE</font></p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley  </p>
+            </div>
         </div>
         
-        <div class="col-sm-6" style="padding:10px">
-        <p><font size="5px">SNORKELIING ADVENTURE</font></p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley  </p>
-        </div>
-        
-        <div class="col-sm-3">
-            <p><center>from USD</center></p>
-            <h2><center>$100</center></h2>
-            <p><a class="btn btn-primary btn-md btn-block" href="#" role="button">BOOK NOW</a></p>
+        <div class="tours-price">
+            <div class="col-sm-3">
+                <p><center>from USD</center></p>
+                <h2><center>$100</center></h2>
+                <p><a class="btn btn-primary btn-md btn-block" href="#" role="button">BOOK NOW</a></p>
+            </div>
         </div>
     </div>
     
