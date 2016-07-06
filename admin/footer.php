a89a49be366a5cbec96efb64f7df4bfc0b60084e
Kenold Beauplan <bkenold@gmail.com>
2016-07-05 21:16:32 -0400

    initial commit

--- admin/footer.php
+++ admin/footer.php
@@ -1,8 +1,5 @@
-<div class="container">
-    <div class="alert alert-info">
-        <p>&copy; 2016 - Viia Tours</p>
     </div>
-</div>
-<script src="../js/bootstrap.min.js"></script>
+    <!-- /container -->
+     
 </body>
 </html>
