4e8ddebddb4b897c0f57af05f97946a196b02ed1
Kenold Beauplan <bkenold@gmail.com>
2016-07-05 21:16:23 -0400

    archive old admin files

--- admin/index.php
+++ admin/index.php
@@ -1,40 +1,109 @@
 <?php
-include_once 'dbconfig.php';
+$page_title = "Tours Listing";
+include_once "header.php";
+
+// create product button
+echo "<div class='right-button-margin'>";
+	echo "<a href='create_product.php' class='btn btn-default pull-right'>Add new tour</a>";
+echo "</div>";
+
+// page given in URL parameter, default page is one
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+// set number of records per page
+$records_per_page = 10;
+
+// calculate for the query LIMIT clause
+$from_record_num = ($records_per_page * $page) - $records_per_page;
+
+
+// include database and object files
+include_once 'config/database.php';
+include_once 'objects/product.php';
+include_once 'objects/category.php';
+
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+
+$product = new Product($db);
+
+// query products
+$stmt = $product->readAll($page, $from_record_num, $records_per_page);
+$num = $stmt->rowCount();
+
+// display the products if there are any
+if($num>0){
+			
+	$category = new Category($db);
+		
+	echo "<table class='table table-hover table-responsive table-bordered'>";
+		echo "<tr>";
+			echo "<th>Tour Name</th>";
+			echo "<th>Price</th>";
+			//echo "<th>Description</th>";
+			echo "<th>Category</th>";
+			echo "<th>Actions</th>";
+		echo "</tr>";
+		
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+		
+			extract($row);
+			
+			echo "<tr>";
+				echo "<td>{$name}</td>";
+				echo "<td>{$price}</td>";
+				//echo "<td>{$description}</td>";
+				echo "<td>";
+					$category->id = $category_id;
+					$category->readName();
+					echo $category->name;
+				echo "</td>";
+				
+				echo "<td>";
+					echo "<a href='update_product.php?id={$id}' class='btn btn-default left-margin'>Edit</a>";
+					echo "<a delete-id='{$id}' class='btn btn-default delete-object'>Delete</a>";
+				echo "</td>";
+				
+			echo "</tr>";
+			
+			//$x++;
+		}
+		
+	echo "</table>";
+	
+	// paging buttons
+	include_once 'paging_product.php';
+}
+
+// tell the user there are no products
+else{
+	echo "<div>No products found.</div>";
+}
 ?>
-<?php include_once 'head.php'; ?>
 
-    <div class="clearfix"></div>
+<script>
+$(document).on('click', '.delete-object', function(){
+		
+	var id = $(this).attr('delete-id');
+	var q = confirm("Are you sure?");
+	
+	if (q == true){
 
-    <div class="container">
-        <a href="add-tour.php" class="btn btn-large btn-info"><i class="glyphicon glyphicon-plus"></i> &nbsp; Add New Tour</a>
-    </div>
+		$.post('delete_product.php', {
+			object_id: id
+		}, function(data){
+			location.reload();
+		}).fail(function() {
+			alert('Unable to delete.');
+		});
 
-    <div class="clearfix"></div><br />
+	}
+		
+	return false;
+});
+</script>
 
-    <div class="container">
-        <table class='table table-hover table-responsive'>
-            <tr>
-                <th>Tour Name Name</th>
-                <th>Price</th>
-                <th colspan="2" align="center">Actions</th>
-            </tr>
-            <?php
-            $query = "SELECT * FROM tbl_tours";
-            $records_per_page=10;
-            $newquery = $crud->paging($query,$records_per_page);
-            $crud->dataview($newquery);
-            ?>
-            <tr>
-                <td colspan="7" align="center">
-                    <div class="pagination-wrap">
-                        <?php $crud->paginglink($query,$records_per_page); ?>
-                    </div>
-                </td>
-            </tr>
-
-        </table>
-
-
-    </div>
-
-<?php include_once 'footer.php'; ?>
+<?php
+include_once "footer.php";
+?>
